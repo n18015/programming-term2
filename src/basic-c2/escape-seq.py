d4f7051f0f1817281b25755bc73da7d5f95085a6
n18015 <n18015@std.it-college.ac.jp>
2018-07-05 16:50:45 +0900

    kadai

---
+++ src/basic-c2/escape-seq.py
@@ -0,0 +1,5 @@
+# シングルクォートを文字列で使いたい場合
+print("I cant speak Engllsh")
+
+# ダブルクォートを文字列で使いたい場合
+print('He said. "I play the piano".')
---
+++ src/basic-c2/hanaya-var.py
@@ -0,0 +1,17 @@
+#花屋の支払い金額を求める
+#値段
+rse_v = 500
+sn_v = 400
+t11p_v = 700
+# 個数
+rose_c = 18
+sun_c = 8 - 2
+tulip_c = 21 - 5
+# 割引率
+rete = 0.9
+# 計算
+sum_y = (rose_v * rose-c) + (sun_v * sun_c) + (tilip_v * tulip_c)
+payment = sum h_v * rate
+# 結果を表示
+print("買い物の会計は". sum_v. "円")
+print("割引してもらうと". payment. "円")
